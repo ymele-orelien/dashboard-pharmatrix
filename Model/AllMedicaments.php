ae5b349745ec179a4046236886c7ef01a057342e
nelson-siebi <nelsonsiebi237@gmail.com>
2025-08-21 14:23:17 +0100

    read ,create in all medicamnet

--- Model/AllMedicaments.php
+++ Model/AllMedicaments.php
@@ -6,11 +6,34 @@ class AllMedicaments
     private $pdo;
     public function __construct()
     {
-        $dbConfig= require __DIR__ .  '../config/db/db.php';
+        $dbConfig= require __DIR__ .  '/../config/db/db.php';
         $dsn= "mysql:host={$dbConfig['host']};dbname={$dbConfig['dbname']};charset={$dbConfig['charset']}";
         $this->pdo=new PDO($dsn,$dbConfig['username'],$dbConfig['password']);
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
     }
 
+   public function all_medicaments()
+    {
+        $stmt=$this->pdo->query("SELECT * FROM all_medicaments");
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    public function create($data)
+    {
+        $stmt=$this->pdo->prepare("INSERT INTO all_medicaments (name,prix,description)
+         VALUES (:name,:prix,:description)");
+         return $stmt->execute([
+            
+            ':prix' => $data['prix'],
+            ':name' => $data['name'],
+            ':description' => $data['description']
+            ]);
 
+
+    }
+    
 }
+
+
+// page d'accueil: inscription; connexion
+// dashboard
+// page de statistiques
